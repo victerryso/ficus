8092d8bc9c070e4ccdc046fd3f17327883efd668
Terry So <victerry.so@gmail.com>
2024-12-07 02:17:30 +1100

    updated ot use npm package

--- src/components/FamilyTree.tsx
+++ src/components/FamilyTree.tsx
@@ -1,59 +1,53 @@
 /* eslint-disable */
 // @ts-nocheck
-import { useEffect } from "react";
-import "../lib/d3.min.js";
-import "../lib/family-tree.min.js";
-import "../lib/family-tree.css";
+import React, { useEffect } from "react";
+import f3 from "family-chart";
+import "family-chart/styles/family-chart.css";
 import csv from "../assets/family-members.csv";
 import { transformCsvToD3Data } from "../lib/transformations.js";
 
-const create = (data) => {
-  if (document.querySelector("#FamilyChart svg")) {
-    return;
-  }
-
-  const cont = document.querySelector("#FamilyChart");
-
-  const store = f3.createStore({
-    data,
-    node_separation: 250,
-    level_separation: 150,
-  });
-  const svg = f3.createSvg(cont);
-  const Card = f3.elements.Card({
-    store,
-    svg,
-    card_dim: {
-      w: 220,
-      h: 70,
-      text_x: 75,
-      text_y: 15,
-      img_w: 60,
-      img_h: 60,
-      img_x: 5,
-      img_y: 5,
-    },
-    card_display: [(d) => `${d.data["first name"]} ${d.data["last name"]}`],
-    mini_tree: true,
-    link_break: false,
-  });
-
-  store.setOnUpdate((props) =>
-    f3.view(store.getTree(), svg, Card, props || {})
-  );
-  store.updateTree({ initial: true });
-};
-
 const FamilyTree = () => {
   useEffect(() => {
-    const d3Data = transformCsvToD3Data(csv);
-    create(d3Data);
+    if (document.querySelector("#FamilyChart svg")) {
+      return;
+    }
+
+    const store = f3.createStore({
+      data: transformCsvToD3Data(csv),
+      node_separation: 250,
+      level_separation: 150,
+    });
+    const svg = f3.createSvg(document.querySelector("#FamilyChart"));
+    const Card = f3.elements.Card({
+      store,
+      svg,
+      card_dim: {
+        w: 220,
+        h: 70,
+        text_x: 75,
+        text_y: 15,
+        img_w: 60,
+        img_h: 60,
+        img_x: 5,
+        img_y: 5,
+      },
+      card_display: [
+        (i) => `${i.data["first name"] || ""} ${i.data["last name"] || ""}`,
+        (i) => `${i.data.birthday || ""}`,
+      ],
+      mini_tree: true,
+      link_break: false,
+    });
+    store.setOnUpdate((props) =>
+      f3.view(store.getTree(), svg, Card, props || {})
+    );
+    store.updateTree({ initial: true });
   }, []);
 
   return (
     <div
-      id="FamilyChart"
       className="f3"
+      id="FamilyChart"
       style={{
         width: "100%",
         height: `${window.screen.height}px`,
