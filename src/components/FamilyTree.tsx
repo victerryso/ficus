4c4361bdfc2714e3232d090d7b715d6da1d800de
Terry So <victerry.so@gmail.com>
2024-12-12 02:05:59 +1100

    updated family-chart version

--- src/components/FamilyTree.tsx
+++ src/components/FamilyTree.tsx
@@ -6,6 +6,29 @@ import "family-chart/styles/family-chart.css";
 import csv from "../assets/family-members.csv";
 import { transformCsvToD3Data } from "../utils/transformations.js";
 
+const createFamilyTree = (data) => {
+  const f3Chart = f3
+    .createChart("#FamilyChart", data)
+    .setTransitionTime(1000)
+    .setCardXSpacing(250)
+    .setCardYSpacing(150)
+    .setOrientationVertical();
+
+  const f3Card = f3Chart
+    .setCard(f3.CardHtml)
+    .setCardDisplay([["first name", "last name"]])
+    .setCardDim({})
+    .setMiniTree(true)
+    .setStyle("imageCircle")
+    .setOnHoverPathToMain();
+
+  f3Card.setOnCardClick((e, d) => {
+    f3Card.onCardClickDefault(e, d);
+  });
+
+  f3Chart.updateTree({ initial: true });
+};
+
 const FamilyTree = () => {
   useEffect(() => {
     if (document.querySelector("#FamilyChart svg")) {
@@ -14,70 +37,7 @@ const FamilyTree = () => {
 
     const data = transformCsvToD3Data(csv);
 
-    // const f3Chart = f3
-    //   .createChart("#FamilyChart", data)
-    //   .setTransitionTime(1000)
-    //   .setCardXSpacing(250)
-    //   .setCardYSpacing(150);
-
-    // const f3Card = f3Chart
-    //   .setCard(f3.CardHtml)
-    //   .setCardDisplay([["first name", "last name"], ["birthday"]])
-    //   .setCardDim({})
-    //   .setMiniTree(true)
-    //   .setStyle("imageRect")
-    //   .setOnHoverPathToMain();
-
-    // const f3EditTree = f3Chart
-    //   .editTree()
-    //   .fixed(true)
-    //   .setFields(["first name", "last name", "birthday"])
-    //   .setEditFirst(true);
-
-    // f3EditTree.setNoEdit();
-
-    // f3Card.setOnCardClick((e, d) => {
-    //   f3EditTree.open(d);
-    //   if (f3EditTree.isAddingRelative()) return;
-    //   f3Card.onCardClickDefault(e, d);
-    // });
-
-    // f3Chart.updateTree({ initial: true });
-    // f3EditTree.open(f3Chart.getMainDatum());
-
-    // f3Chart.updateTree({ initial: true });
-
-    const store = f3.createStore({
-      data: transformCsvToD3Data(csv),
-      node_separation: 250,
-      level_separation: 150,
-    });
-    const svg = f3.createSvg(document.querySelector("#FamilyChart"));
-    const Card = f3.elements.Card({
-      store,
-      svg,
-      card_dim: {
-        w: 220,
-        h: 70,
-        text_x: 75,
-        text_y: 15,
-        img_w: 60,
-        img_h: 60,
-        img_x: 5,
-        img_y: 5,
-      },
-      card_display: [
-        (i) => `${i.data["first name"] || ""} ${i.data["last name"] || ""}`,
-        (i) => `${i.data.birthday || ""}`,
-      ],
-      mini_tree: true,
-      link_break: false,
-    });
-
-    store.setOnUpdate((props) =>
-      f3.view(store.getTree(), svg, Card, props || {})
-    );
-    store.updateTree({ initial: true });
+    createFamilyTree(data);
   }, []);
 
   return (
